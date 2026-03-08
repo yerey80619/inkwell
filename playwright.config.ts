bf17989bb46a53ad1bf574adfcd4f2fecdc5f734
yerey <yerey80619@gmail.com>
2026-03-08 01:28:22 -0600

    Update playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [["html", { outputFolder: "test-artifacts/results" }]],
+  outputDir: "test-artifacts/test-output",
+
+  use: {
+    baseURL: "http://localhost:3000",
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+  },
+
+  projects: [
+    {
+      name: "setup",
+      testMatch: /auth\.setup\.ts/,
+    },
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: "test-artifacts/.auth/user.json",
+      },
+      dependencies: ["setup"],
+    },
+  ],
+});
